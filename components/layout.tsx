f245e7e13736b3f670fdb28eb1831fa55586e26b
Deepak Prabhakara <deepak@boxyhq.com>
2022-03-20 02:30:52 +0000

    updated nextauth (#3)

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,6 +1,5 @@
 import Header from "./header"
 import Footer from "./footer"
-import type { ReactChildren } from "react"
 
 interface Props {
   children: React.ReactNode
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,46 +1,21 @@
 import NextAuth from "next-auth"
+import BoxyHQSAMLProvider from "next-auth/providers/boxyhq-saml"
 
-const samlLoginUrl = process.env.BOXYHQ_SAML_JACKSON_URL || "http://localhost:5225";
+const samlLoginUrl =
+  process.env.BOXYHQ_SAML_JACKSON_URL || "http://localhost:5225"
 
 // For more information on each option (and a full list of options) go to
 // https://next-auth.js.org/configuration/options
 export default NextAuth({
   // https://next-auth.js.org/configuration/providers/oauth
   providers: [
-    {
-      id: "boxyhq",
-      name: "BoxyHQ SAML Jackson",
-      type: "oauth",
-      version: "2.0",
-      checks: ["pkce", "state"],
-      authorization: {
-        url: `${samlLoginUrl}/api/oauth/authorize`,
-        params: {
-          scope: "",
-          response_type: "code",
-          provider: "saml",
-        },
-      },
-      token: {
-        url: `${samlLoginUrl}/api/oauth/token`,
-        params: { grant_type: "authorization_code" },
-      },
-      userinfo: `${samlLoginUrl}/api/oauth/userinfo`,
-      profile: (profile) => {
-        return {
-          id: profile.id || "",
-          firstName: profile.firstName || "",
-          lastName: profile.lastName || "",
-          email: profile.email || "",
-          name: `${profile.firstName || ""} ${profile.lastName || ""}`.trim(),
-          email_verified: true,
-        };
-      },
-      options: {
-        clientId: `tenant=boxyhq.com&product=${process.env.BOXYHQ_PRODUCT || "next-auth-demo"}`,
-        clientSecret: "dummy",
-      },
-    },
+    BoxyHQSAMLProvider({
+      issuer: samlLoginUrl,
+      clientId: `tenant=boxyhq.com&product=${
+        process.env.BOXYHQ_PRODUCT || "next-auth-demo"
+      }`,
+      clientSecret: "dummy",
+    }),
   ],
   theme: {
     colorScheme: "light",
